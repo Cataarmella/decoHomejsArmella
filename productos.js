77046f1bcb3c28ce01e374baa83d0ed9216e9c59
Catalina Armella <cataarmella@gmail.com>
2022-10-26 16:02:39 -0300

    nuevo proyecto

---
+++ productos.js
@@ -0,0 +1,38 @@
+const productos = [{
+    id: 1,
+    foto: "./img/canastopalma.jpg",
+    nombre: "Canasto palma",
+    precio: 7590
+},
+{
+    id: 2,
+    foto: "./img/canastopalmatapa.jpg",
+    nombre: "Canasto palma tapa",
+    precio: 8750
+},
+{
+    id: 3,
+    foto: "./img/espejoginebra.jpg",
+    nombre: "Espejo Ginebra",
+    precio: 60000
+},
+{
+    id: 4,
+    foto: "./img/velabubble.jpg",
+    nombre: "Vela Bubble",
+    precio: 2000
+},
+{
+    id: 5,
+    foto: "./img/velamaia.jpg",
+    nombre: "Vela Maia",
+    precio: 3000
+},
+{
+    id: 6,
+    foto:"./img/velarainbow.jpg", 
+    nombre: "Vela Rainbow",
+    precio: 2100
+    
+}
+];
